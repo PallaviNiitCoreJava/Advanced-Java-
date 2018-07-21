b5c3ac2850dff0eff09737f13b6a9ffcfc4270a0
PallaviNiitCoreJava <37013933+PallaviNiitCoreJava@users.noreply.github.com>
2018-07-21 14:55:40 +0530

    Add files via upload

---
+++ ConfigContextExample/src/java/one/Cart.java
@@ -0,0 +1,25 @@
+package one;
+
+public class Cart {
+    
+    String prod;
+    String decs;
+
+    public String getProd() {
+        return prod;
+    }
+
+    public void setProd(String prod) {
+        this.prod = prod;
+    }
+
+    public String getDecs() {
+        return decs;
+    }
+
+    public void setDecs(String decs) {
+        this.decs = decs;
+    }
+
+
+}
