c46a4b51ecec20a1954b276d5764d65bb236501d
PallaviNiitCoreJava <37013933+PallaviNiitCoreJava@users.noreply.github.com>
2018-09-05 10:54:16 +0530

    Add files via upload

---
+++ JPAExample/build/generated-sources/ap-source-output/one/Student_.java
@@ -0,0 +1,16 @@
+package one;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Student.class)
+public abstract class Student_ {
+
+	public static volatile SingularAttribute<Student, Long> id;
+	public static volatile SingularAttribute<Student, String> studName;
+	public static volatile SingularAttribute<Student, String> Stud_std;
+
+}
+
